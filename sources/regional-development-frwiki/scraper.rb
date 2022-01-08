a6e9b0c121d5d15228f3423f8eabda182fbdc9fd
Tony Bowden <tony@tmtm.com>
2022-01-08 16:02:20 +0200

    compare frwiki Ministers of Regional Development

---
+++ sources/regional-development-frwiki/scraper.rb
@@ -0,0 +1,34 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Image'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[image name party color cabinet named start end].freeze
+    end
+
+    def empty?
+      tds[1].text == tds[2].text
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
